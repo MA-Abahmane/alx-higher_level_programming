fd9bb259ef0f6a959bb1d50fe405f72ea2ecec9d
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-09 15:19:25 +0100

    P1

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def islower(c):
-   
+
     x = ord('a')
     if (type(c) == int or c == ''):
         print("Traceback (most recent call last):")
-    
+
     while (chr(x) != chr(ord('z') + 1)):
         if (chr(x) == c):
             return True
