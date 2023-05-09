c98b588cff41ae4e699ed498f28a66cacb9dd8c7
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-09 15:09:55 +0100

    P1

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,8 @@
 
 for x in range(0, 11):
     for y in range(0, 10):
-        if ( x == 8 and y == 9 ):
-            print( "{0}{1}".format(x ,y) )
-        if ( y > x ):
-            print( "{0}{1}".format(x ,y), end= ", " )
+        if (x == 8 and y == 9 ):
+            print("{0}{1}".format(x ,y))
+            break
+        if (y > x):
+            print("{0}{1}".format(x ,y), end=", ")
