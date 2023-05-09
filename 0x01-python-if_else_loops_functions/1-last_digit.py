7c8a281af2077b307cba5fbdeb25df5114020036
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-09 14:52:28 +0100

    P1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 
 last_dig = ((abs(number) % 10)
 # in case of a negative
-if number < 0:
+if (number < 0):
     last_dig *= -1
 
 print("Last digit of {0} is {1}".format(number, last_dig), end=" ")
