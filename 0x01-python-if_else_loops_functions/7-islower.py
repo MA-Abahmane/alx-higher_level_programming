be5d6bf15211e3bf892fbb4d11860d4ce5feed59
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-09 14:00:10 +0100

    P1

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
 
+def islower(c):
+    x = ord('a')
+
+    while ( chr(x) != chr( ord('z') + 1) ):
+        if (chr(x) == c):
+            return True
+        x += 1
+
+    return False
