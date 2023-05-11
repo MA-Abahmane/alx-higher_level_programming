6a23468a27c34d150a2dc634650e3cd8e9b1e2cc
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-11 13:46:54 +0100

    P1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-from add_0 import add
 
-a = 1
-b = 2
-result = add(a, b)
+def add_ab():
+  a = 1
+  b = 2
+  result = add(a, b)
 
-print("{0} + {1} = {2}".format(a , b, result))
+  print("{0} + {1} = {2}".format(a , b, result))
+
+# if file not run as main; do not run.
+if __name__ == '__main__':
+  from add_0 import add
+  add_ab
