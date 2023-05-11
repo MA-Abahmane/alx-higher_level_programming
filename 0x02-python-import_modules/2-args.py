32c05720c0906e8e35574f273de9f7476260dc34
MA-Abahmane <ma.abahmane@gmail.com>
2023-05-11 13:42:12 +0000

    P1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,23 +1,30 @@
-#!/usr/bin/python3
-import sys
+#!/usr/bin/python4
 
-argc = len(sys.argv)
-argv = sys.argv
+def main_calc():
 
+  if (argc > 1):
 
-if (argc > 1):
+    if (argc == 2):
+      print("{0} argument:".format(1))
+    else:
+      print("{0} arguments:".format(argc - 1))
 
-  if (argc == 2):
-    print("{0} argument:".format(1))
+    i = 1
+    while (i < argc):
+      argument = argv[i]
+      print("{0}: {1}".format(i, argument))
+
+      i += 1
+
   else:
-    print("{0} arguments:".format(argc - 1))
-  
-  i = 1
-  while (i < argc):
-    argument = argv[i]
-    print("{0}: {1}".format(i, argument))
+      print("{0} arguments.".format(0))
 
-    i += 1
 
-else:
-    print("{0} arguments.".format(0))
+# if file not run as main; do not run.
+if __name__ == '__main__':
+  import sys
+
+  argc = len(sys.argv)
+  argv = sys.argv
+
+  def main_calc(argc, argv):
