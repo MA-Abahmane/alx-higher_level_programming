6ee72dec77168f3b205ce208d3509311b3b25c7d
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-11 13:53:25 +0100

    P1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-from file import add, sub, mul, div
 
-a = 10
-b = 5
+simple_calc(a, b):
 
-add_result = add(a, b)
-sub_result = sub(a, b)
-mul_result = mul(a, b)
-div_result = div(a, b)
+  # process
+  add_result = add(a, b)
+  sub_result = sub(a, b)
+  mul_result = mul(a, b)
+  div_result = div(a, b)
 
-print(str(a) + " + " + str(b) + " = " + str(add_result))
-print(str(a) + " - " + str(b) + " = " + str(sub_result))
-print(str(a) + " * " + str(b) + " = " + str(mul_result))
-print(str(a) + " / " + str(b) + " = " + str(div_result))
+  # output
+  print(str(a) + " + " + str(b) + " = " + str(add_result))
+  print(str(a) + " - " + str(b) + " = " + str(sub_result))
+  print(str(a) + " * " + str(b) + " = " + str(mul_result))
+  print(str(a) + " / " + str(b) + " = " + str(div_result))
+
+# if file not run as main; do not run.
+if __name__ == '__main__':
+  from file import add, sub, mul, div
+  
+  a = 10
+  b = 5
+  simple_calc(a, b)
