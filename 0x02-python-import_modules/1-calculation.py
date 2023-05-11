589784b7a37b3a52a2335f5d7c764e87d854e572
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-11 13:56:06 +0100

    P1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -16,7 +16,7 @@ def simple_calc(a, b):
 
 # if file not run as main; do not run.
 if __name__ == '__main__':
-  from file import add, sub, mul, div
+  from calculator_1 import add, sub, mul, div
   
   a = 10
   b = 5
