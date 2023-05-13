730899e52a0da57ed7ab5ecc651791e3bbf18bdf
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-13 11:58:31 +0100

    P1

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,13 @@
 
 def print_matrix_integer(matrix=[[]]):
 
-    for list in matrix:
-        for num in list:
-            print("{:d}".format(num), end=" ")
+    mother_list = (len(matrix) - 1)
+    child_list = len(matrix[0])
+    for x in mother_list:
+        for y in child_list:
+            if (y == child_list):
+                print("{:d}".format(num), end="")
+            else:
+                print("{:d}".format(num), end=" ")
         print()
 
