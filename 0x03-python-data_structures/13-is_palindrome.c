2847abf12e768518242f3d2e18d2114588ee9479
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-14 13:11:57 +0100

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,48 +10,48 @@
 int is_palindrome(listint_t **head)
 {
 int i = 0, j = 0, arr_size = 0;
-  listint_t  *ptr;
+listint_t  *ptr;
 
   /* if list is empty, is palindrom */
-  if (head == NULL)
-    return 1;
+if (head == NULL)
+return 1;
   
   /* find out how many numbers in the list */
-  ptr = *head;
-  while (ptr != NULL)
-    {
-      arr_size++;
-      ptr = ptr->next;
-    }
+ptr = *head;
+while (ptr != NULL)
+{
+arr_size++;
+ptr = ptr->next;
+}
 
-  /* set array size */
-  int *num_arr = NULL;
-  num_arr = malloc(4 * arr_size);
+/* set array size */
+int *num_arr = NULL;
+num_arr = malloc(4 * arr_size);
 
-  /* add each number to my array */
-  ptr = *head;
-  i = 0;
-  while (ptr != NULL)
-    {
-      num_arr[i] = ptr->n;
-      ptr = ptr->next;
-      i++;
-    }
+/* add each number to my array */
+ptr = *head;
+i = 0;
+while (ptr != NULL)
+{
+num_arr[i] = ptr->n;
+ptr = ptr->next;
+i++;
+}
 
-  /* check if my array makes a palindrop */
-  i = 0;
-  j = (arr_size - 1);
-  while (i < arr_size - 1 && j >= 0)
-    {
-      if (num_arr[i] != num_arr[j])
-      {
-        free(num_arr);
-        return 0;
-      }
-      i++;
-      j--;
-    }
+/* check if my array makes a palindrop */
+i = 0;
+j = (arr_size - 1);
+while (i < arr_size - 1 && j >= 0)
+{
+if (num_arr[i] != num_arr[j])
+{
+free(num_arr);
+return 0;
+}
+i++;
+j--;
+}
 
-  free(num_arr);
-  return 1;
+free(num_arr);
+return 1;
 }
