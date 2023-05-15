da3bbde839148219e246a29122a73658e3030920
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-15 15:28:40 +0100

    PYC

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,21 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <Python.h>
+
+/**
+ * print_python_list_info - a C function that prints some basic info about Python lists.
+ * @p: pointer to a pointer to a Python list object.
+ *
+ * Return: NONE
+ */
 
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t i = 0, size_p, allocated_ptr;
-  
-	PyObject *data;
+Py_ssize_t i, p_len, allocated_ptrs;
+PyObject *data;
 
-	size_p = PyList_Size(p);
-	allocated_ptr = ((PyListObject *)p)->allocated;
-  
-	printf("[*] Size of the Python List = %ld\n", size_p);
-	printf("[*] Allocated = %ld\n", allocated_ptr);
-  
-	while (i < size_p)
-	{
-		data = PyList_GET_ITEM(p, i);
-		printf("Element %ld: %s\n", i, data->ob_type->tp_name);
-		i++;
-	}
+/* get the length of the Pyhton list object p, works like len(p) */
+p_len = PyList_Size(p);
+
+/* get the number on pointer to python list objs that are allocated */
+allocated_ptrs = ((PyList_Size)*p)->allocated;
+
+/* output */
+print("[*] Size of the Python List = %d", size);
+print("[*] Allocated = %d", allocated_ptrs);
+
+/* from list object 'p' get each elements with ptr 'data' with index 'i' */
+/* then get elements type name 'data_type'/print element type and its index */
+for (i = 0; i < p_len; i++)
+{
+    
+data = PyList_GET_ITEM(p, i);
+const char* data_type = data->ob_type->tp_name;
+
+printf("Element %ld: %s\n", i, data_type);  
+}
 }
