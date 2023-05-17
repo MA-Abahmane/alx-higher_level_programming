16d4834f8d984b450815038b75a54583438a0b83
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-17 17:12:16 +0100

    P1

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-
-    dict_cpy = a_dictionary.copy()
     #get each key/value from dict, then multiply its value
-    for key, val in dict_cpy.items():
-        dict_cpy[key] = val*2
-
-    return (dict_cpy)
+    return {key: val*2 for key, val in a_dictionary.items()}
