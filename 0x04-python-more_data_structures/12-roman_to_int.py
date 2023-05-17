2049253f95740d5e3889f89c4fee4e45a1dfdb3b
MA-Abahmane <ma.abahmane@gmail.com>
2023-05-17 19:19:34 +0000

    P1

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+
+    if (roman_string is None or type(roman_string) != str):
+        return (0)
+
+    Rnum = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    prev_n = 0
+    n = 0
+
+    for c in roman_string:
+        Cval = Rnum[c]
+
+        if (prev_n < Cval):
+            n -= (2 * prev_n)
+
+        n += Rnum[c]
+        prev_n = n
+
+    return (n)
