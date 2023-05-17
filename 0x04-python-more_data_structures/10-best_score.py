0862416e05f5c298b7cd5ca373bbe796238c74b1
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-17 17:20:31 +0100

    P1

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,12 +3,14 @@
 def best_score(a_dictionary):
     if (a_dictionary == None):
         return None
-    
+
     score = 0
-    #get each key/value from dict, then multiply its value
+    # get each keyvalue from dict then multiply its value
     for i in a_dictionary:
         if (a_dictionary[i] > score):
             score = a_dictionary[i]
             winner_name = i
-    
+    if (score <= 0):
+        return None
+
     return (winner_name)
