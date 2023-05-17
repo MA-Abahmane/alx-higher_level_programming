23310f1c741d9e6a4832a5379db6611676b9bc5d
MA-Abahmane <ma.abahmane@gmail.com>
2023-05-17 19:40:51 +0000

    Nothing here

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,13 @@
+#include <Python.h>
+#inclide <stdio.h>
+#inclide <stdlib.h>
+
+void print_python_list(PyObject *p);
+    PyObject *x;
+    x = p
+    return(0)
+
+void print_python_bytes(PyObject *p);
+    PyObject *y;
+    y = p
+    return(0)
