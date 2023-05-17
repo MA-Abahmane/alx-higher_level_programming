780b0ffecca6de5c1bfc3242107b1c102825b358
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-17 17:08:10 +0100

    Delete 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++
@@ -1,8 +0,0 @@
-def multiply_by_2(a_dictionary):
-
-    dict_cpy = a_dictionary.copy()
-    #get each key/value from dict, then multiply its value
-    for key, val in dict_cpy.items():
-        dict_cpy[key] = val*2
-
-    return (dict_cpy)
