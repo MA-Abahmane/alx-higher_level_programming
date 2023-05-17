38651e57d7822474c997f9da0fc219f42848e250
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-17 20:44:04 +0100

    Update 103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,13 +1,17 @@
 #include <Python.h>
-#inclide <stdio.h>
-#inclide <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void print_python_list(PyObject *p);
-    PyObject *x;
-    x = p
-    return(0)
+void print_python_list(PyObject *p)
+{
+PyObject *x;
+x = p
+return(0)
+}
 
-void print_python_bytes(PyObject *p);
-    PyObject *y;
-    y = p
-    return(0)
+void print_python_bytes(PyObject *p)
+{
+PyObject *y;
+y = p
+return(0)
+}
