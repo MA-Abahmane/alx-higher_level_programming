4b8eba26ec8c5745dd668191b76bb3d8799996b4
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-29 15:28:06 +0100

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@
 def safe_function(fct, *args):
     from sys import stderr as err 
     try:
-        rzlt = fct(args[0], args[1])
+        rzlt = fct(*args)
     except Exception as error:
         print("Exception: {}".format(error), file=err)
         return (None)
