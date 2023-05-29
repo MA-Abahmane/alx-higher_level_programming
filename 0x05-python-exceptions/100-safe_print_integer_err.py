8291162b8753fdf4f857843065abe5674d7d19e7
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-29 15:20:12 +0100

    PY

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_integer_err(value):
-
+    from sys import atderr
     try:
         print("{:d}".format(value))
-    except Exception as ex:
-        print(f"Exception: {ex}")
+    except Exception as error:
+        print("Exception: {}".format(error), file = stderr)
         return (False)
     else:
         return (True)
