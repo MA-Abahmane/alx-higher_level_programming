206445d1b3d49ec3a605858b7f91ef24e1a21188
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-30 15:02:00 +0100

    PY1

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+""" class Square ckeck if size in a digit and is >= to 0 """
+
 
 class Square:
-    """ class Square ckeck if size in a digit and is >= to 0 """
+    """ The constructor """
+    
     def __init__(self, size=0):
-        """ The constructor """
         if (type(size) != int):
             raise ValueError("size must be an integer")
         elif (size < 0):
