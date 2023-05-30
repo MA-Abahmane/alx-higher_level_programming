5c1617cf53f6296544059bf5cf71ba37f2cef7c7
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-30 15:09:15 +0100

    PY1

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
+""" The class Square """
+
 
 class Square:
-    """ class Square ckeck if size in a digit and is >= to 0 """
+    """ The constructor """
+
     def __init__(self, size=0):
-        """ The constructor """
         self.size = size
-    
+
+
+    """ Get size """
+
     @property
     def size(self):
-        """ Get size """
         return self.__size
-    
+
+
+    """ Get size """
+
     @size.setter
     def size(self, value):
-        """ Get size """
         if (type(value) != int):
             raise ValueError("size must be an integer")
         elif (value < 0):
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
-        
+
+
+ """ Area calculator """
+
     def area(self):
-        """ Area calculator """
         return self.__size ** 2
