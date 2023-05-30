d7526ea6db0ca89043af67588a62750f05319de2
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-30 15:38:08 +0100

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,33 +1,39 @@
 #!/usr/bin/python3
+"""The class Square"""
+
 
 class Square:
-    """ class Square ckeck if size in a digit and is >= to 0 """
+    """inside class Square"""
+
     def __init__(self, size=0):
-        """ The constructor """
+        """initialise squares.
+
+        Args:
+            size (int): square size.
+        """
         self.size = size
-    
+
     @property
     def size(self):
-        """ Get size """
+        """Set size as a private instance attributes."""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
-        """ Get size """
+        """Get size"""
         if (type(value) != int):
             raise ValueError("size must be an integer")
         elif (value < 0):
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
-        
+
     def area(self):
-        """ Area calculator """
+        """Area calculator"""
         return self.__size ** 2
-    
+
     def my_print(self):
-        """ hash printer """
+        """ printing function """
         value = self.size
         if (value == 0):
             print()
