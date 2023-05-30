c69cbcee97fdd7206af65d38b0de5d7bdc0f7d72
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-30 23:27:14 +0100

    Update 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-import math
 """ Class MagicClass """
+import math
 
 
 class MagicClass:
@@ -12,14 +12,24 @@ class MagicClass:
          radius (int): given radius value
       """
       self.__radius = 0
-      if (type(radius) is not int and type(radius) is not float):
-          raise TypeError('radius must be a number')
-      self.__radius = radius
 
+    @property
+    def radius(self):
+        """ return the radius """
+        return self.radius
+
+    @radius.setter
+    def radius(self, val):
+        """ set radius """
+        if (type(val) is not int and type(val) is not float):
+            raise TypeError('radius must be a number')
+        else:
+            self.__radius = val
+    
     def area(self):
-      """Disassembly of area:"""
-      return (self.__radius ** 2 * math.pi)
+        """ Disassembly of area: """
+        return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-      """Disassembly of circumference:"""
-      return (self.__radius * math.pi * 2)
+        """ Disassembly of circumference: """
+        return (self.__radius * math.pi * 2)
