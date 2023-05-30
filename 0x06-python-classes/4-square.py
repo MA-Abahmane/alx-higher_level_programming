1855e6d052ca4ddb2613e637d931cca798e51e3b
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-30 15:21:54 +0100

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,23 +3,24 @@
 
 
 class Square:
-    """ The constructor """
+    """ inside class Square """
 
     def __init__(self, size=0):
+        """ initialise squares
+
+        Args:
+        size (int): square suze.
+        """
         self.size = size
-
-
-    """ Get size """
 
     @property
     def size(self):
+        """ set size as a private instance attributes """
         return self.__size
-
-
-    """ Get size """
 
     @size.setter
     def size(self, value):
+          """ check if size is a digit and in not a negative one """
         if (type(value) != int):
             raise ValueError("size must be an integer")
         elif (value < 0):
@@ -27,8 +28,6 @@ class Square:
         else:
             self.__size = value
 
-
- """ Area calculator """
-
     def area(self):
+         """ Area calculator """
         return self.__size ** 2
