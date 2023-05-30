27bb65c6b7a94755b1b846c69b2a2a1e913f5c1d
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-30 23:35:22 +0100

    Update 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,7 +7,11 @@ class MagicClass:
     """ inside MagicClass """
 
     def __init__(self, radius=0):
-      """ Disassembly of __init__: """
+      """ Disassembly of __init__: 
+ 
+      Args:
+        radius (int): given radius value"""
+
       self.__radius = 0
 
       if (type(radius) is not int and type(radius) is not float):
@@ -16,9 +20,9 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
-      """Disassembly of area:"""
+      """ Disassembly of area: """
       return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-      """Disassembly of circumference:"""
+      """ Disassembly of circumference: """
       return (self.__radius * math.pi * 2)
