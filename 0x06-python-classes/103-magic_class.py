da72472ca79bbfcaa598a8d44538f6fdf445e471
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-30 23:32:22 +0100

    Update 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -6,31 +6,18 @@ import math
 class MagicClass:
 
     def __init__(self, radius=0):
-      """ Disassembly of __init__: 
-
-      Args:
-         radius (int): given radius value
-      """
-      self.radius = radius
+      """ Disassembly of __init__: """
       self.__radius = 0
 
-    @property
-    def radius(self):
-        """ return the radius """
-        return self.radius
+      if (type(radius) is not int and type(radius) is not float):
+          raise TypeError('radius must be a number')
+      else:
+        self.__radius = radius
 
-    @radius.setter
-    def radius(self, val):
-        """ set radius """
-        if (type(val) is not int and type(val) is not float):
-            raise TypeError('radius must be a number')
-        else:
-            self.__radius = val
-    
     def area(self):
-        """ Disassembly of area: """
-        return (self.__radius ** 2 * math.pi)
+      """Disassembly of area:"""
+      return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-        """ Disassembly of circumference: """
-        return (self.__radius * math.pi * 2)
+      """Disassembly of circumference:"""
+      return (self.__radius * math.pi * 2)
