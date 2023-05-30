70b9eb197eac6ad22b9185e4279b6ba77037691f
MA-Abahmane <ma.abahmane@gmail.com>
2023-05-30 22:07:48 +0000

    PY2

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+""" Class MagicClass """
+import math.pi
+
+
+class MagicClass:
+
+    def __init__(self, radius=0):
+      """ Disassembly of __init__: """
+      self.__radius = 0
+
+      if (type(radius) is not int and type(radius) is not float):
+          raise TypeError('radius must be a number')
+      else:
+        self.__radius = radius
+
+    def area(self):
+      """Disassembly of area:"""
+      return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+      """Disassembly of circumference:"""
+      return (self.__radius * math.pi * 2)
