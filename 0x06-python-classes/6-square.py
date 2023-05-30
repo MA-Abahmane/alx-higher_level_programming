db402a76956ecf19e4f49d2e47b1d529752d3fe8
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-30 19:27:15 +0100

    PY1

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -52,7 +52,7 @@ class Square:
         return (self.__size ** 2)
 
     def my_print(self):
-        """ printing function """
+        """ Printing function """
         value = self.size
 
         if (value == 0):
