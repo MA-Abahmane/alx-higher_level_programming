8672db64a29cd4c5bcdcc3bec7930f586cf1da75
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-30 23:20:38 +0100

    Update 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -14,8 +14,7 @@ class MagicClass:
       self.__radius = 0
       if (type(radius) is not int and type(radius) is not float):
           raise TypeError('radius must be a number')
-      else:
-        self.__radius = radius
+      self.__radius = radius
 
     def area(self):
       """Disassembly of area:"""
