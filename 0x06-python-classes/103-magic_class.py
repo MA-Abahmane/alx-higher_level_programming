923a5f22d8a66021aa2c73f6a8aeede91dfe851f
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-30 23:38:02 +0100

    PY2

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,22 +7,22 @@ class MagicClass:
     """ inside MagicClass """
 
     def __init__(self, radius=0):
-      """ Disassembly of __init__: 
+        """ Disassembly of __init__: 
  
-      Args:
-        radius (int): given radius value"""
+        Args:
+            radius (int): given radius value"""
 
-      self.__radius = 0
+        self.__radius = 0
 
-      if (type(radius) is not int and type(radius) is not float):
-          raise TypeError('radius must be a number')
-      else:
-        self.__radius = radius
+        if (type(radius) is not int and type(radius) is not float):
+            raise TypeError('radius must be a number')
+        else:
+            self.__radius = radius
 
     def area(self):
-      """ Disassembly of area: """
-      return (self.__radius ** 2 * math.pi)
+        """ Disassembly of area: """
+        return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-      """ Disassembly of circumference: """
-      return (self.__radius * math.pi * 2)
+        """ Disassembly of circumference: """
+        return (self.__radius * math.pi * 2)
