3ab41ce1b497da4dcfbd22b2bce4fd9707e66048
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-30 15:44:42 +0100

    PY1

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python3
+"""The class Square"""
+
 
 class Square:
-    """ The class Square """
+    """inside class Square"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """ The constructor """
+        """initialise squares.
+
+        Args:
+            size (int): square size.
+        """
         self.size = size
         self.positions = position
-    
+
     @property
     def size(self):
-        """ Get size """
+        """ Set size as a private instance attributes """
         return (self.__size)
-    
+
     @size.setter
     def size(self, value):
-        """ Get size """
+        """ Check if the value passes is a positive digit """
         if (type(value) != int):
             raise ValueError("size must be an integer")
         elif (value < 0):
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
+
     @property
     def position(self):
-        """ Get positions """
+        """ Set position as a private instance attributes """
         return (self.__positions)
-    
+
     @position.setter
     def position(self, value):
-        """ Get position """
+      """ Check if the value passes is a tuple with 2 digits """
         if (type(value) != tuple or len(value) != 2):
             raise ValueError("position must be a tuple of 2 positive integers")
         elif (value[0] and value[1] < 0 or type(value[0]) and type(value[1]) != int):
@@ -42,15 +48,15 @@ class Square:
     def area(self):
         """ Area calculator """
         return (self.__size ** 2)
-    
+
     def my_print(self):
-        """ hash printer """
+        """ printing function """
         value = self.size
         x, y = self.positions
 
         if (value == 0):
             print()
-        
+
         for i in range(y):
             print()
         for j in range(0, value):
