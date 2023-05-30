2474919dbcfa6be921743418fb86042339dc47d6
MA-Abahmane <ma.abahmane@gmail.com>
2023-05-30 18:29:51 +0000

    PY!

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""The class Square"""
+
+
+class Node:
+    """inside class Square"""
+
+    def __init__(self, data, next_node=None):
+        """initialise squares.
+
+        Args:
+            data (int): ssquare size.
+            next_node (node): given space indexs
+        """
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """ data property  """
+        return (self.__data)
+
+    @data.setter
+    def data(self, value): 
+        """ data setter """
+        if (type(value) != int):
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        """ data property  """
+        return (self.__next_node)
+
+    @next_node.setter
+    def next_node(self, value): 
+        """ data setter """
+        if (type(value) != Node and value is not None):
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__next_node = value
+
+
+
+    def my_print(self):
+        """ printing function """
+        value = self.size
+    
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
