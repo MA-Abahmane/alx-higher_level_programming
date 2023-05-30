dd4439de730c8aded88b9701dc2dda0bedf82b08
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-30 15:05:12 +0100

    PY1

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+""" class Square now return the area of the number entered """
+
 
 class Square:
-    """ class Square ckeck if size in a digit and is >= to 0 """
+    """ The constructor """
+
     def __init__(self, size=0):
-        """ The constructor """
         if (type(size) != int):
             raise ValueError("size must be an integer")
         elif (size < 0):
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-        
+
+    """ Area calculator """
+
     def area(self):
-        """ Area calculator """
         return self.__size ** 2
