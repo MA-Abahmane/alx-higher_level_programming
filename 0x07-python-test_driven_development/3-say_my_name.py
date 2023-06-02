1a022837299b0411f967ee212cc1941126c82dd3
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-02 11:16:14 +0100

    PY1

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+""" This module has a function that devides all matrix elememts """
+
 
 def say_my_name(first_name, last_name=""):
+    """ The function is given a first/last name to print
+    
+    Args:
+        first_name (str): given first name
+        last_name (str): given last name
+        
+    Returns:
+        My name is (first/last name) """
 
     if (isinstance(first_name, str) is False):
         raise TypeError('first_name must be a string')
