1329c16884cdbae1659de011f495bca0cbf93909
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-02 18:10:55 +0100

    Py1

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
-""" This module has a function that prints a square out of '#' """
+""" This module has a function that prints a indents strings """
 
 
-def print_square(size):
-    """ The function that prints a square of '#' with 
-    a given size
+def text_indentation(text):
+    """  a function that prints a text with 2 new lines
+    after each of these characters: ., ? and :
 
     Args:
-        size (int): given is the size length of the square
+        text (str): given string to indent
     """
+    if (isinstance(text, str) is False):
+        raise TypeError('text must be a string')
+    
+    char_list = ['.', '?', ':']
+
+    chars = list(text)
+    for i in range(0, len(chars)):
+        if (chars[i] in char_list):
+            print(chars[i], end='')
+            print('\n')
+        elif (chars[i - 1] in char_list and chars[i] == ' '):
+            continue
+        else:
+            print(chars[i], end='')
