a5fb3d8caf1eeb3a2a0be150ac3ca5be555b889e
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-04 21:01:21 +0100

    PY2

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "Python.h"
+
+
+void print_python_string(PyObject *p)
+{
+
+unsigned int len;
+char *str;
+
+/* Flush the standard output */
+fflush(stdout);
+
+
+printf("[.] string object info\n");
+
+/* If p is not a valid string */
+if (strcmp(p->ob_type->tp_name, "str") != 0)
+{
+printf("  [ERROR] Invalid String Object\n");
+return;
+}
+
+/* retreaving the length of the string */
+len = ((PyASCIIObject*)(p))->length;
+
+/* Check if the string is a ASCII compact string */
+/* range (0, 127) */
+if (!PyUnicode_IS_COMPACT_ASCII(p))
+{
+printf(" type: compact unicode object\n");
+}
+else
+{
+printf(" type: compact ascii\n");
+}
+
+/* get the string value by converting it to a wide */
+/* character string in case of special chars ex:(在, щ) */
+str = PyUnicode_AsWideCharString(p, &len);
+printf("  length: %d\n", len);
+printf("  value: %s\n", str);
