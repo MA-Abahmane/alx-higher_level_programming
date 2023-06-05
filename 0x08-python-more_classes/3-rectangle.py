6b0a72658b5b952ce377a6f411f7f37fb8f20bb0
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-06-05 16:38:20 +0100

    PY1

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -44,7 +44,6 @@ class Rectangle:
 
         self.__height = value
 
-
     def perimeter(self):
         """ returns the rectangle perimeter """
         if (self.__height == 0 or self.__width == 0):
@@ -55,7 +54,6 @@ class Rectangle:
         """ returns the rectangle area """
         return (self.__height * self.__width)
 
-
     def my_str(self):
         """ string building method """
         str = ""
@@ -64,7 +62,7 @@ class Rectangle:
                 str += ('#')
             str += ('\n')
         return (str)
-    
+
     def printer(self):
         """ string printing function """
         print(self.my_str(), end='')
