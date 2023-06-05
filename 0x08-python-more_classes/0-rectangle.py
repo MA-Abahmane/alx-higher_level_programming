8836168901c250a676318d57382a60af4c817ffa
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-05 16:29:34 +0100

    PY1

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" Empty class Rectangle that defines a rectangle: """
-
+""" Empty class Rectangle that defines a rectangle: 
+"""
 
 
 class Rectangle:
     """ Inside class Rectangle """
+
     pass
