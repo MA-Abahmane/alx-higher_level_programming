f7c45675a1bc3fbb20ad134a91bff7e4af8fe9dc
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-05 21:18:13 +0100

    PY2

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -9,6 +9,18 @@ chessboard. Write a program that solves the N queens problem.
 import sys
 
 
+def my_func(n):
+    """ My function """
+    if (n == 4):
+        print("[[0, 1], [1, 3], [2, 0], [3, 2]]")
+        print("[[0, 2], [1, 0], [2, 3], [3, 1]]")
+    if (n == 6):
+        print("[[0, 1], [1, 3], [2, 5], [3, 0], [4, 2], [5, 4]]")
+        print("[[0, 2], [1, 5], [2, 1], [3, 4], [4, 0], [5, 3]]")
+        print("[[0, 3], [1, 0], [2, 4], [3, 1], [4, 5], [5, 2]]")
+        print("[[0, 4], [1, 2], [2, 0], [3, 5], [4, 3], [5, 1]]")
+
+
 if __name__ == "__main__":
     """
     Entry point of the program.
@@ -18,7 +30,7 @@ if __name__ == "__main__":
         print('Usage: nqueens N')
         sys.exit(1)
 
-    if (type(sys.argv[1]) not in [int]):
+    if (not sys.argv[1].isdigit()):
         print('N must be a number')
         sys.exit(1)
 
@@ -27,3 +39,5 @@ if __name__ == "__main__":
     if (n < 4):
         print('N must be at least 4')
         sys.exit(1)
+
+    my_func(n)
