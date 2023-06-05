ff90693752f8e20f55a4f9a9a899a09682be6ef1
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-05 19:54:24 +0100

    PY1

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,1 +1,25 @@
+#!/usr/bin/python3
+""" The N queens puzzle is the challenge of placing
+N non-attacking queens on an N×N chessboard. Write 
+a program that solves the N queens problem. """
+
+
+from sys import argv
+
+
+if (len(argv) != 2):
+    print('Usage: nqueens N')
+    exit(1)
+
+n = int(argv[1])
+
+if (type(n) not in [int]):
+    print('N must be a number')
+    exit(1)
+
+if (n < 4):
+    print('N must be at least 4')
+    exit(1)
+
+
 
