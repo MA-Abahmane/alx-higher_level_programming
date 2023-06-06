0cf4bf60201da325bbb421baac1c8cbd25a7aa75
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-06 12:48:22 +0100

    PY1

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+""" 
+Write a function magic_string() that returns a string 
+“BestSchool” n times the number of the iteration (see code): 
+"""
+
+
 class LockedClass:
+    """ class contains a function returning a string """
+
     names_allowed = ['first_name']
+
     def __setattr__(self, name , value):
+        """ returns a string “BestSchool” n times the number of the iteration """
+
         if name not in self.names_allowed:
             print('[AttributeError] \'LockedClass\' object has no attribute \'last_name\'')
