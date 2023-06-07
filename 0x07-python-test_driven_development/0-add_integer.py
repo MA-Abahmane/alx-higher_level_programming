0ed96f685ee9a0df793c95a0c191ae3411cc7d1c
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-07 12:15:06 +0100

    PY_Testing

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-""" This module has the function 'add_integer' used for addition """
+""" Write a function that adds 2 integers.
+Prototype: def add_integer(a, b=98):
+a and b must be integers or floats, otherwise raise a TypeError exception
+a and b must be first casted to integers if they are float
+Returns an integer: the addition of a and b """
 
 
 def add_integer(a, b=98):
     """ This function takes in 2 digits and returns their sum
-
-    Args:
-        a (int): first number
-        b (int): second number
-    Returns:
-        int: The sum of a and b """
+    Args: a (int): first number /b (int): second number
+    Returns: int: The sum of a and b """
 
     if (type(a) not in [int, float]):
         raise TypeError('a must be an integer')
     if (type(b) not in [int, float]):
         raise TypeError('b must be an integer')
 
-    if (type(a) in [float]):
+    if (type(a) is float):
         a = int(a)
-    if (type(b) in [float]):
+    if (type(b) is float):
         b = int(b)
 
     return (a + b)
