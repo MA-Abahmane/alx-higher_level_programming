0f98f71b034f67a4a182d9da5faeaa948a36b82d
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-07 13:12:56 +0100

    PY_Testing

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This mudul contains a function that
+multiplies 2 matrices by using the module NumPy
+"""
+import numpy as npy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ Yup, this is the one """
+
+    result = npy.multiply(m_a, m_b)
+    return (result)
