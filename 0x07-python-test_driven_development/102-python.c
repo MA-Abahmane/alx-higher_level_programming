c93034d87b96e153903c4721c88c2cf989313882
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-07 12:38:11 +0100

    Print PY string in C

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,36 +1,47 @@
 #include <stdio.h>
-#include <string.h>
 #include <Python.h>
+#include <string.h>
 
-void print_python_string(PyObject *p) {
-    unsigned int len;
-    const char *str;
+/*
+This is a function that is dedicated to printing Python strings.
+*/
 
-    /* Flush the standard output */
-    fflush(stdout);
 
-    printf("[.] string object info\n");
 
-    /* If p is not a valid string */
-    if (!PyUnicode_Check(p)) {
-        printf("  [ERROR] Invalid String Object\n");
-        return;
-    }
+void print_python_string(PyObject *p)
+{
+unsigned int len;
+const char *str;
 
-    /* retrieving the length of the string */
-    len = PyUnicode_GET_LENGTH(p);
+/* Flush the standard output */
+fflush(stdout);
 
-    /* Check if the string is a ASCII compact string */
-    /* range (0, 127) */
-    if (!PyUnicode_IS_COMPACT_ASCII(p)) {
-        printf("  type: compact unicode object\n");
-    }
-    else {
-        printf("  type: compact ascii\n");
-    }
+printf("[.] string object info\n");
 
-    /* get the string value */
-    str = PyUnicode_AsUTF8(p);
-    printf("  length: %u\n", len);
-    printf("  value: %s\n", str);
+/* Check If p is a valid string */
+if (!PyUnicode_Check(p))
+{
+printf("  [ERROR] Invalid String Object\n");
+return;
+}
+
+/* retrieving the length of the string */
+len = PyUnicode_GET_LENGTH(p);
+
+/* Check if the string is a ASCII compact string */
+/* [!] compact string: range (0, 127) */
+if (!PyUnicode_IS_COMPACT_ASCII(p))
+{
+printf("  type: compact unicode object\n");
+}
+else
+{
+printf("  type: compact ascii\n");
+}
+
+/* get the string values and print */
+str = PyUnicode_AsUTF8(p);
+printf("  length: %u\n", len);
+printf("  value: %s\n", str);
+
 }
