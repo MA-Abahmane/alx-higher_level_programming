78086ac43b87c999c5365d13ef232ff68a53f64d
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-07 12:25:12 +0100

    PY_Testing

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-""" Write a function that adds 2 integers.
-Prototype: def add_integer(a, b=98):
-a and b must be integers or floats, otherwise raise a TypeError exception
-a and b must be first casted to integers if they are float
-Returns an integer: the addition of a and b """
+"""
+Write a function that adds 2 integers.
+    Prototype: def add_integer(a, b=98):
+    a and b must be integers or floats, otherwise raise a TypeError exception
+    a and b must be first casted to integers if they are float
+    Returns an integer: the addition of a and b
+"""
 
 
 def add_integer(a, b=98):
-    """ This function takes in 2 digits and returns their sum
-    Args: a (int): first number /b (int): second number
-    Returns: int: The sum of a and b """
+    """
+    This function takes in 2 digits and returns their sum
+    Args:
+        a (int): first number
+        b (int): second number
+
+    Returns:
+        int: The sum of a and b
+    """
 
     if (type(a) not in [int, float]):
         raise TypeError('a must be an integer')
@@ -21,4 +29,4 @@ def add_integer(a, b=98):
     if (type(b) is float):
         b = int(b)
 
-    return (a + b)
+    return a + b
