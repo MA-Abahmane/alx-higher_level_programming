f43575a0e84e7a6c7e72fb1a96c403b15c818c82
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-06-12 15:52:01 +0100

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-""" a class BaseGeometry (based on 5-base_geometry.py """
+""" 
+a class BaseGeometry (based on 5-base_geometry.py
+"""
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-""" Class Square """
+""" 
+Class Square
+"""
 
 
 class Square(Rectangle):
-    """ Inside class Square that inherits  from class Rectangle """
-
+    """ Inside class Square that inherits class Rectangle """
     def __init__(self, size):
         """ square constructor """
         self.__size = size
-
-        # Running the superclass (Rectangle) constructor:
-        # super() is suited for this operation
         super().__init__(self.__size, self.__size)
