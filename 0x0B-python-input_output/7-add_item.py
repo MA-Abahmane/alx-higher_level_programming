824991b64e43c87094e03abc72c259ff716d2822
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-06-13 15:06:29 +0100

    PY1

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 from sys import argv
-"""access commandline ar"""
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-"""create"""
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-"""wri"""
 
-filename = "add_item.json"
+"""
+    a script that adds all arguments to a Python list,
+    and then save them to a file:
+"""
 
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+""" load list from add_item.json, if list not found; creat empy one """
+fileName = 'add_item.json'
 try:
-    content = load_from_json_file(filename)
-except:
-    content = []
+    list = load_from_json_file(fileName)
+except Exception:
+    list = []
 
+""" append user arguments to list """
 for i in range(1, len(argv)):
-    content.append(argv[i])
+    list.append(argv[i])
 
-save_to_json_file(content, filename)
+""" overwrite the files content (update add_item.json) """
+save_to_json_file(list, fileName)
