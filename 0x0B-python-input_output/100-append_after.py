4a78c207542d52595f9f3a27799b8e6d0661c972
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-13 15:59:23 +0100

    PY1

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    a function that inserts a line of text to a file,
+    after each line containing a specific string:
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ fist, we open the file to read mode and save its lines """
+    with open(filename, 'r', encoding="utf-8") as fl:
+        lns = fl.readlines()
+
+    new_lns = []
+
+    """ 
+        now we look for the given search string in each line
+        if found, add the new string right after that line
+    """
+    for ln in lns:
+        new_lns.append(ln)
+
+        if (search_string in ln):
+            new_lns.append(new_string)
+
+    """ lastly, we add our new lines to the file after clearing it """
+    with open(filename, 'w', encoding="utf-8") as fle:
+        fle.writelines(new_lns)
