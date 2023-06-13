2d282ca5b421b8e0731b96e14b0b292e46626185
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-06-13 15:05:54 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 from sys import argv
+"""access commandline ar"""
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+"""create"""
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+"""wri"""
 
-"""
-    a script that adds all arguments to a Python list,
-    and then save them to a file:
-"""
+filename = "add_item.json"
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
-
-""" load list from add_item.json, if list not found; creat empy one """
-fileName = 'add_item.json'
 try:
-    list = load_from_json_file(fileName)
-except Exception:
-    list = []
+    content = load_from_json_file(filename)
+except:
+    content = []
 
-""" append user arguments to list """
 for i in range(1, len(argv)):
-    list.append(argv[i])
+    content.append(argv[i])
 
-""" overwrite the files content (update add_item.json) """
-save_to_json_file(list, fileName)
+save_to_json_file(content, filename)
