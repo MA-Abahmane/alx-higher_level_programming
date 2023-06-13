20f44ddc949e8a5c570a6969493e1da131644b01
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-13 16:16:36 +0100

    f

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,18 +7,3 @@ import json
 from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
-
-""" load list from add_item.json, if list not found; creat empy one """
-fileName = 'add_item.json'
-try:
-    list = load_from_json_file(fileName)
-except Exception:
-    list = []
-
-""" append user arguments to list """
-for i in range(1, len(argv)):
-    list.append(argv[i])
-
-""" overwrite the files content (update add_item.json) """
-save_to_json_file(list, fileName)
