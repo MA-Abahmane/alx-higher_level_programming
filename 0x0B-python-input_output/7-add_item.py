54bac730af7ef59b870a6dfb0c9ba35e7dc81b35
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-06-13 15:00:10 +0100

    s

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 from sys import argv
-"""access commandline arguments"""
+
+"""
+    a script that adds all arguments to a Python list,
+    and then save them to a file:
+"""
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-"""create object from JSON file"""
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-"""writes an object to text file, using JSON representation"""
 
 
 """ load list from add_item.json, if list not found; creat empy one """
-fileName = 'add_item.json'
+fileName = "add_item.json"
 try:
     list = load_from_json_file(fileName)
-except Exception:
+except:
     list = []
 
 """ append user arguments to list """
