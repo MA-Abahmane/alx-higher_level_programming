e547018e9025904e178c876cf199b942fb63bc57
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-13 20:06:22 +0100

    PY1

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
     Technical interview preparation:
     Create a function def pascal_triangle(n): that returns a list
     of lists of integers representing the Pascal’s triangle of n:
@@ -20,13 +20,16 @@ def pascal_triangle(n):
         crnt_rw.append(1)
 
         for i in range(1, row + 1):
+            # if end of row is reached
             if (i is row):
-                 crnt_rw.append(1)
+                crnt_rw.append(1)
                 # add 1 at the end
             else:
+                # get the correct sequence number
                 num = (pascalT[row - 1][i - 1] + pascalT[row - 1][i])
                 crnt_rw.append(num)
 
         # add row to the pascal list
         pascalT.append(crnt_rw)
+
     return (pascalT)
