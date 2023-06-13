40ee8ff996e03a89c772bcf404b330b6bb84e4f6
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-13 14:52:26 +0100

    PY1

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 from sys import argv
+
 """
     a script that adds all arguments to a Python list,
     and then save them to a file:
 """
-
 
 """ loading needed json functions """
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-"""
-    load list from add_item.json,
-    if list not found; creat empy one
-"""
+""" load list from add_item.json, if list not found; creat empy one """
+fileName = 'add_item.json'
 try:
-    list = load_from_json_file('add_item.json')
+    list = load_from_json_file(fileName)
 except Exception:
     list = []
 
@@ -25,4 +23,4 @@ for i in range(1, len(argv)):
     list.append(argv[i])
 
 """ overwrite the files content (update add_item.json) """
-save_to_json_file(list, 'add_item.json')
+save_to_json_file(list, fileName)
