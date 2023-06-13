bf2b95630b18ec3244beb96b92f9564ed1dc805d
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-13 20:01:43 +0100

    PY1

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """ 
     Technical interview preparation:
     Create a function def pascal_triangle(n): that returns a list
@@ -6,13 +7,26 @@
 
 
 def pascal_triangle(n):
-    list = []
+    """ Creat the Pascal Triangle """
+    pascalT = []
 
     if (n <= 0):
-        return list
-    
-    a = 1
-    b = 1
-    c = 1
-    for i in range(n):
-        lst = []
+        return pascalT
+
+    for row in range(n):
+        crnt_rw = []
+
+        # add 1 at the biginning
+        crnt_rw.append(1)
+
+        for i in range(1, row + 1):
+            if (i is row):
+                 crnt_rw.append(1)
+                # add 1 at the end
+            else:
+                num = (pascalT[row - 1][i - 1] + pascalT[row - 1][i])
+                crnt_rw.append(num)
+
+        # add row to the pascal list
+        pascalT.append(crnt_rw)
+    return (pascalT)
