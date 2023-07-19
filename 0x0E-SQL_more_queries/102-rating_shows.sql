c8e7e6f4c8dbf5e3209677c96d3aa23e087e89fa
Mohamed Amine <ma.abahmane@gmail.com>
2023-07-19 20:41:19 +0100

    SQL

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,2 @@
+-- a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+
---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,2 @@
+-- a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+
