96b2195fb1be3025a344348641adc3e6b1924a45
Mohamed Amine <ma.abahmane@gmail.com>
2023-07-20 10:44:22 +0100

    SQL

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,2 +1,19 @@
 -- a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
 
+CREATE TABLE IF NOT EXISTS genres(
+    `name` VARCHAR(100),
+    `rate` INT
+);
+
+INSERT INTO genres VALUES 
+    ("Drama", 150), 
+    ("Comedy", 92), 
+    ("Adventure", 79), 
+    ("Fantasy", 79),
+    ("Mystery", 45), 
+    ("Crime", 40), 
+    ("Suspense", 40), 
+    ("Thriller", 40);
+
+SELECT `name`, `rate` AS `rating` FROM genres
+ORDER BY `rate` DESC;
