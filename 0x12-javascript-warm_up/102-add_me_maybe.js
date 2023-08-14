8139e4b875a442fda82071be8f3574c4eb97e63e
Mohamed Amine <ma.abahmane@gmail.com>
2023-08-14 22:56:25 +0100

    js

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* a function that increments and calls a function. */
 
-exports.addMeMaybe  = function (number, theFunction) {
+exports.addMeMaybe = function (number, theFunction) {
   number++;
   theFunction(number);
-}
+};
