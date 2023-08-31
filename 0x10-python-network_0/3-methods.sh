14e3bd32c023ef327dc20c003fd0d1cbfe093f8b
Your Name <you@example.com>
2023-08-31 07:13:23 -0700

    PyWeb

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -sI "$1" | grep -i "^Allow:"
+curl -sI "$1" | grep -i "^Allow:" | cut -b 8-
