4cab21b07925ac486c793c540057d913c7b5098c
Mohamed Amine <ma.abahmane@gmail.com>
2023-08-31 14:50:06 +0100

    PyNet

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  A Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-response=$(curl -s -w "${http_code}" "$1"); [ "${response: -3} -eq 200" ] && curl -s "$1" || echo "${response: -3}"
+curl -sX GET "$1" -L
