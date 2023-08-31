39a1e5cbd9985d32d262a72c453fb0d72b357041
Mohamed Amine <ma.abahmane@gmail.com>
2023-08-31 18:07:05 +0100

    PyNet

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response. 
-curl -sX GET 0.0.0.0:5000/catch_me
+link="0.0.0.0:5000/catch_me"; curl -sw "You got me!" $link
