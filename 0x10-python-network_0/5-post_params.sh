9e9e06f998db86f661001f6cec5d98ce9b7088df
Your Name <you@example.com>
2023-08-31 07:39:46 -0700

    PyNet

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sX POST "$1" -H "email: test@gmail.com" -H "subject: 'I will always be here for PLD'"
+curl -sX POST "$1" -d "email=test@gmail.com" -d "subject=I will always be here for PLD" -L
