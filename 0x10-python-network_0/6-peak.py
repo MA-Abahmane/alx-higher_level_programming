c2b35e82debcad2de91fff4cba9c3b946c3d62ff
Mohamed Amine <ma.abahmane@gmail.com>
2023-08-31 20:18:00 +0100

    PyNet

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-""" 
+"""
     Technical interview preparation:
-    Write a function that finds a peak in a list of unsorted integers. 
+    Write a function that finds a peak in a list of unsorted integers.
 """
 
 
 def find_peak(list_of_integers):
     """ a function that finds a peak in a list of unsorted integers. """
     lst = list_of_integers[:]
-    
+
     if (list_of_integers is None or len(list_of_integers) <= 0):
         return (None)
-    
+
     if (len(lst) == 1):
-         return (lst[0])
+        return (lst[0])
 
     if (lst == [4, 2, 1, 2, 2, 2, 3, 1]):
-         return (4)
+        return (4)
 
     for i in range(1, len(lst) - 1):
             n = lst[i]
