9b68688cf6ad8feb9e807ead40bda99f208a2fba
Mohamed Amine <ma.abahmane@gmail.com>
2023-09-01 20:54:43 +0100

    PyNet

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,10 +9,10 @@ from urllib.request import urlopen, Request
 if __name__ == "__main__":
     """ fetches https://alx-intranet.hbtn.io/status """
 
-    url = 'https://alx-intranet.hbtn.io/status'
+    req = Request('https://alx-intranet.hbtn.io/status')
 
-    with urlopen(url) as response:
+    with urlopen(req) as response:
         details = response.read()
         print('Body response:')
-        print(f'\t- type: {type(details)}\n\t- content: {details}\
-              \n\t- utf8 content: {details.decode("utf-8")}')
+        print(f"\t- type: {type(details)}\n\t- content: {details}\
+              \n\t- utf8 content: {details.decode("utf-8")}")
