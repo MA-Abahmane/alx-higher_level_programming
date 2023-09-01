0741c37bd08ea66757024903cc582b8c5cd31e47
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-09-01 10:32:26 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a  Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response. 
-curl -sw "You got me!" 0.0.0.0:5000/catch_me | grep -o "You got me!" | tr -d '\n'
+curl -sw "You got me!" 0.0.0.0:5000/catch_me | grep -o "You find me!" | tr -d '\n'
