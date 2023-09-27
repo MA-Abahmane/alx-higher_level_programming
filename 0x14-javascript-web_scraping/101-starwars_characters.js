0d3358b5f2b372495599886a4b1220e9c1eed601
Mohamed Amine <ma.abahmane@gmail.com>
2023-09-27 23:17:35 +0100

    Updated

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -13,28 +13,7 @@ request(URL, function (error, response, body) {
   // if no errors and the request was OK; start process
   body = JSON.parse(body);
 
-  if (!error) {
-    console.log('Luke Skywalker');
-    console.log('C-3PO');
-    console.log('R2-D2');
-    console.log('Darth Vader');
-    console.log('Leia Organa');
-    console.log('Obi-Wan Kenobi');
-    console.log('Chewbacca');
-    console.log('Han Solo');
-    console.log('Jabba Desilijic Tiure');
-    console.log('Wedge Antilles');
-    console.log('Yoda');
-    console.log('Palpatine');
-    console.log('Boba Fett');
-    console.log('Lando Calrissian');
-    console.log('Ackbar');
-    console.log('Mon Mothma');
-    console.log('Arvel Crynyd');
-    console.log('Wicket Systri Warrick');
-    console.log('Nien Nunb');
-    console.log('Bib Fortuna');
-  } else {
+  if (error) {
     console.log(error);
   }
 });
